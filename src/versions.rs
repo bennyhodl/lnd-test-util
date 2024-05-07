96d2975ba7bdce94faa6cd994f9a7fd0e26bd2b2
benthecarman <benthecarman@live.com>
2024-05-07 15:51:59 -0500

    Use lnd 17.5

--- src/versions.rs
+++ src/versions.rs
@@ -4,17 +4,17 @@ const OS: &str = "macos";
 #[cfg(target_os = "linux")]
 const OS: &str = "linux";
 
-#[cfg(feature = "lnd_0_17_3")]
-const VERSION: &str = "v0.17.3-beta";
+#[cfg(feature = "lnd_0_17_5")]
+const VERSION: &str = "v0.17.5-beta";
 
 #[cfg(feature = "lnd_16_4")]
 const VERSION: &str = "v0.16.4-beta";
 
-#[cfg(not(any(feature = "lnd_0_17_3", feature = "lnd_0_16_4",)))]
+#[cfg(not(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4",)))]
 
 const VERSION: &str = "NA";
 
-pub const HAS_FEATURE: bool = cfg!(any(feature = "lnd_0_17_3", feature = "lnd_0_16_4",));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4",));
 
 pub fn lnd_name() -> String {
     VERSION.to_string()
