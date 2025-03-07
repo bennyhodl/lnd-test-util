af6f305f52fb3eaa0dfb5a1539c824bf052ef5b6
benthecarman <benthecarman@live.com>
2025-03-06 20:31:52 -0600

    Update deps

--- src/versions.rs
+++ src/versions.rs
@@ -4,17 +4,20 @@ const OS: &str = "darwin";
 #[cfg(target_os = "linux")]
 const OS: &str = "linux";
 
+#[cfg(feature = "lnd_0_18_5")]
+const VERSION: &str = "v0.18.5-beta";
+
 #[cfg(feature = "lnd_0_17_5")]
 const VERSION: &str = "v0.17.5-beta";
 
 #[cfg(feature = "lnd_16_4")]
 const VERSION: &str = "v0.16.4-beta";
 
-#[cfg(not(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4",)))]
+#[cfg(not(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4", feature = "lnd_0_18_5")))]
 
 const VERSION: &str = "NA";
 
-pub const HAS_FEATURE: bool = cfg!(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4",));
+pub const HAS_FEATURE: bool = cfg!(any(feature = "lnd_0_17_5", feature = "lnd_0_16_4", feature = "lnd_0_18_5"));
 
 pub fn lnd_name() -> String {
     // todo: add arm64 and armv7 support
